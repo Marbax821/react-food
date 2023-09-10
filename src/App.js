63aeb37fd28bf1801ca65b20122ba405023c25ef
Marbax <macintosh@MacBook-Air.local>
2023-09-10 19:15:21 +0200

    added basename

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/react-food'>
       <div>
         <Header />
         <main className='container content'>
